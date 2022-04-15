787bb00fe0a26722448368491b92f794aaaedff4
mario mendonca <mariomeendonca00@gmail.com>
2022-04-15 10:41:19 -0300

    remove not used vars and functions

--- index.py
+++ index.py
@@ -1,5 +1,3 @@
-from cmath import nan
-from numpy import append
 import streamlit as st
 import pandas as pd
 
@@ -34,47 +32,26 @@ def remove_outliers():
         outliers.append(i) 
     
     # genre
-    elif type(data[i][12]) == float or type(data[i][12]) == nan:
+    elif type(data[i][12]) == float:
       outliers.append(i) 
   return outliers
-  # return df.drop(labels=outliers, axis=0)
 
 def remove_missing_values():
-  outliers = []
+  missing_values = []
   data = df.values
   for i in range(len(data)):
     if (type(data[i][12]) == str):
       genre = data[i][12].replace(' ', '')
       if (len(genre) == 0):
-        outliers.append(i)
-  # return df.drop(labels=outliers, axis=0)
-  return outliers
+        missing_values.append(i)
+  return missing_values
 
-# print(df.values[603][12])
-# print(type(df.values[603][12]))
+outliersList = remove_outliers()
+missing_valuesList = remove_missing_values()
+dropList = outliersList + missing_valuesList
+dropList = list(dict.fromkeys(dropList))
+df = df.drop(labels=dropList, axis=0)
 
-array1 = remove_outliers()
-array2 = remove_missing_values()
-
-print(len(array1) + len(array2))
-
-array1 = array1 + array2
-print(len(array1))
-
-mylist = list(dict.fromkeys(array1))
-print(len(mylist))
-
-
-print(len(df))
-df = df.drop(labels=mylist, axis=0)
-print(len(df))
-
-# print(len(df.values))
-# df = remove_outliers()
-# # df = remove_missing_values()
-# remove_missing_values()
-# df = remove_genre_missing_values()
-# print(len(df.values))
 
 option = st.sidebar.selectbox(
   'Escolha o grafico que deseja ver!',
@@ -99,69 +76,3 @@ elif option == 'abandonos':
 elif option == 'dataset':
   st.write('Dataset')
   st.dataframe(df)
-
-
-
-
-# def testing():
-#   for i in range(df.index[-1]):
-#     if (not df['genero'][i]):
-#       print(i)
-
-
-# def remove_outliers():
-#   outliers = []
-#   for i in range(len(df.values)):
-#     if df.values[i][4] > 5:
-#       outliers.append(i)
-#     elif df.values[i][] : 
-
-#   return df.drop(labels=outliers, axis=0)
-
-# def remove_pages_outliers():
-#   df_count = df
-#   outliers = []
-#   for i in range(len(df)):
-#     if df_count['paginas'][i] == 0:
-#       outliers.append(i)
-#   return df_count.drop(labels=outliers, axis=0)
-
-# def remove_sex_outliers():
-#   df_count = df
-#   outliers = []
-#   for i in range(len(df)):
-#     if (df_count['male'][i] + df_count['female'][i] ) != 100:
-#       if (df_count['lendo'][i] != 0) and (df_count['relendo'][i] != 0) and (df_count['leram'][i] != 0):
-#         outliers.append(i)
-#   return df_count.drop(labels=outliers, axis=0)
-
-# def remove_genre_missing_values():
-#   df_count = df
-#   outliers = []
-#   for i in range(len(df)):
-#     if (type(df_count['genero'][i]) == str):
-#       genre = df_count['genero'][i].replace(' ', '')
-#       if (len(genre) == 0):
-#         outliers.append(i)
-#   return df_count.drop(labels=outliers, axis=0)
-
-
-# def remove_values_outliers():
-#   df_count = df
-#   outliers = []
-#   for i in range(len(df)):
-#     if (type(df_count['genero'][i]) == float):
-#       outliers.append(i)
-
-#   return df_count.drop(labels=outliers, axis=0)
-
-# def remove_genre_missing_values():
-#   df_count = df
-#   outliers = []
-#   for i in range(len(df)):
-#     if (type(df_count['genero'][i]) == str):
-#       genre = df_count['genero'][i].replace(' ', '')
-#       if (len(genre) == 0):
-#         outliers.append(i)
-#   return df_count.drop(labels=outliers, axis=0)
-
