58148076a0de191b42ba328d5d8583b887bc1e77
carlosgabriel311 <75148299+carlosgabriel311@users.noreply.github.com>
2022-05-15 11:08:13 -0300

    recomendação por genero

--- genero_algoritmo.py
+++ genero_algoritmo.py
@@ -4,6 +4,7 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from yellowbrick.cluster import SilhouetteVisualizer
 import matplotlib.pyplot as plt
+import random
 
 df = pd.read_csv('data/dados.csv')
 to_drop = ['ISBN_13', 'ISBN_10', 'ano', 'resenha', 'editora']
@@ -66,7 +67,7 @@ def populateNewColumns():
         df[column][existentIndex] = 1
 populateNewColumns()
 def testing():
-  new_df = pd.DataFrame([], columns = ['titulo', 'rating', 'genero'])
+  new_df = pd.DataFrame([], columns = ['titulo', 'genero', 'rating'])
   for i in range(len(df)):
     existentIndex = df.index[i]
     if (type(df['titulo'][existentIndex]) == str):
@@ -76,42 +77,76 @@ def testing():
             new_df = new_df.append({'titulo': int(existentIndex + 1), "rating": df['rating'][existentIndex], "genero": newColumnsList.index(x) + 1}, ignore_index=True)
   return new_df
 new_df = testing()
+
+col = new_df.drop('titulo', axis=1)
+kmeans = KMeans(n_clusters = 4, 
+init = 'k-means++', n_init = 10, 
+max_iter = 300) 
+pred_y = kmeans.fit_predict(col)
+
+new_df['cluster_id'] = kmeans.labels_
+
 st.dataframe(new_df)
 
-
-# X = new_df['genero']
-# y = new_df['rating']
-
-km = KMeans(n_clusters=3, random_state=42)
-km.fit_predict(new_df)
-score = silhouette_score(new_df, km.labels_, metric='euclidean')
-print('Silhouetter Score: %.3f' % score)
-
-# fig, ax = plt.subplots(2, 2, figsize=(15,8))
-# for i in [2, 3, 4, 5]:
-#     km = KMeans(n_clusters=i, init='k-means++', n_init=10, max_iter=100, random_state=42)
-#     q, mod = divmod(i, 2)
-#     visualizer = SilhouetteVisualizer(km, colors='yellowbrick', ax=ax[q-1][mod])
-#     visualizer.fit(new_df)
-#     st.pyplot(plt)
-    
-
-fig, ax = plt.subplots(2, 2, figsize=(15,8))
-km = KMeans(n_clusters=4, init='k-means++', n_init=10, max_iter=100, random_state=42)
-q, mod = divmod(4, 2)
-
-
-plt.scatter(new_df['genero'], new_df['rating'])
-plt.xlim(0, 50) #range do eixo x
-plt.ylim(0, 10) #range do eixo y
-st.pyplot(plt)
-
-kmeans = KMeans(n_clusters = 4, #numero de clusters
-init = 'k-means++', n_init = 10, #algoritmo que define a posição dos clusters de maneira mais assertiva
-max_iter = 300) #numero máximo de iterações
-pred_y = kmeans.fit_predict(new_df)
 plt.scatter(new_df['genero'], new_df['rating'], c = pred_y) #posicionamento dos eixos x e y
 plt.xlim(0, 50) #range do eixo x
-plt.ylim(0, 10) 
+plt.ylim(0, 5) 
 plt.scatter(kmeans.cluster_centers_[:,1],kmeans.cluster_centers_[:,0], s = 70, c = 'red') #posição de cada centroide no gráfico
 st.pyplot(plt)
+
+cluster_zero = []
+cluster_one = []
+cluster_two = []
+cluster_three = []
+
+for i in range(len(new_df.values)):
+  if new_df.values[i][3] == 0:
+    cluster_zero.append(i)
+  elif new_df.values[i][3] == 1:
+    cluster_one.append(i)
+  elif new_df.values[i][3] == 2:
+    cluster_two.append(i)
+  elif new_df.values[i][3] == 3:
+    cluster_three.append(i)
+
+def recomendation_per_id(bookId):
+  book = new_df.values[bookId]
+  cluster_id = book[3]
+  
+  if cluster_id == 0:
+    random_list1 = random.sample(cluster_zero, 30)
+    random_list2 = random.sample(cluster_one, 20)
+    return random_list1 + random_list2
+  
+  elif cluster_id == 1: 
+    random_list1 = random.sample(cluster_one, 30)
+    random_list2 = random.sample(cluster_zero, 10)
+    random_list3 = random.sample(cluster_two, 10)
+    return random_list1 + random_list2 + random_list3
+  
+  elif cluster_id == 2:
+    random_list1 = random.sample(cluster_two, 30)
+    random_list2 = random.sample(cluster_one, 10)
+    random_list3 = random.sample(cluster_three, 10)
+    return random_list1 + random_list2 + random_list3
+  elif cluster_id == 3:
+    random_list1 = random.sample(cluster_three, 30)
+    random_list2 = random.sample(cluster_two, 20)
+    return random_list1 + random_list2
+
+def returning_books(books_array):
+  list_books = []
+  for i in books_array:
+    list_books.append(new_df.values[int(i)])
+  return list_books
+
+
+zero = recomendation_per_id(0)
+one = recomendation_per_id(18)
+three = recomendation_per_id(21)
+two = recomendation_per_id(37)
+
+st.write(returning_books(zero))
+st.write(returning_books(one))
+st.write(returning_books(two))
+st.write(returning_books(three))
